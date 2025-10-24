88bd58a648c7de6cba3e85b41997d6ed066713de
betul <k.betul_98@hotmai.com>
2025-10-24 11:54:17 +0300

    rev5

---
+++ level 4/rev_wstr.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	end;
+	int	start;
+
+	if (ac == 2)
+	{
+		i = 0;
+		while (av[1][i])
+			i++;
+		i--;
+		while (i >= 0)
+		{
+			while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
+				i--;
+			end = i;
+			while (i >= 0 && (av[1][i] != ' ' && av[1][i] != '\t'))
+				i--;
+			start = i + 1;
+			if (start <= end)
+			{
+				write(1, &av[1][start], end - start + 1);
+				if (i > 0)
+					write(1, " ", 1);
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
