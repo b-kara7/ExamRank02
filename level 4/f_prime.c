b4ddcda4a399357640467a124454cd3be927cca5
betul <k.betul_98@hotmai.com>
2025-11-28 07:45:01 +0300

    revizeyüz

--- level 4/f_prime.c
+++ level 4/f_prime.c
@@ -1,47 +1,53 @@
+#include <unistd.h>
 
-#include <stdio.h>
-
-int mini_atoi(char *s)
+void	putnbr(int n)
 {
-    int i = 0;
-    int num = 0;
+	char c;
 
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        num = num * 10 + (s[i] - '0');
-        i++;
-    }
-    return num;
+	if (n >= 10)
+		putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
 }
 
-int main(int ac, char **av)
+int	mini_atoi(char *s)
 {
-    int n;
-    int i = 2;
+	int i = 0;
+	int res = 0;
 
-    if (ac == 2)
-    {
-        n = mini_atoi(av[1]);
-
-        if (n == 1)
-        {
-            printf("1\n");
-            return 0;
-        }
-
-        while (i <= n)
-        {
-            if (n % i == 0)
-            {
-                printf("%d", i);
-                n /= i;
-                if (n > 1)
-                    printf("*");
-            }
-            else
-                i++;
-        }
-    }
-    printf("\n");
-    return 0;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = res * 10 + (s[i] - '0');
+		i++;
+	}
+	return (res);
 }
+
+int	main(int ac, char **av)
+{
+	int n;
+	int i = 2;
+
+	if (ac == 2)
+	{
+		n = mini_atoi(av[1]);
+		if (n == 1)
+			write(1, "1", 1);
+
+		while (i <= n)
+		{
+			if (n % i == 0)
+			{
+				putnbr(i);
+				n /= i;
+				if (n > 1)
+					write(1, "*", 1);
+			}
+			else
+				i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
