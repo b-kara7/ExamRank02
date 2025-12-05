6d75dbba06fc43fd13630be3e5a91b6d7c977b9d
betul <k.betul_98@hotmai.com>
2025-12-05 23:27:19 +0300

    bitartık

--- level 2/ft_last_word.c
+++ level 2/ft_last_word.c
@@ -1,27 +1,53 @@
 #include <unistd.h>
 
+static int is_space(char c)
+{
+    return (c == ' ' || (c >= 9 && c <= 13));
+}
+
 int main(int ac, char **av)
 {
-    int i = 0;
-    int start = 0;
+    int i;
+    int start;
+    int end;
+    char *s;
 
-    if (ac == 2)
+    if (ac != 2)
     {
-        while (av[1][i])
-        {
-            while (av[1][i] == ' ' || av[1][i] == '\t')
-                i++;
-            if (av[1][i]) // yeni kelimenin başlangıcı
-                start = i;
-            while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
-                i++;
-        }
-        while (av[1][start] && av[1][start] != ' ' && av[1][start] != '\t')
-        {
-            write(1, &av[1][start], 1);
-            start++;
-        }
+        write(1, "\n", 1);
+        return (0);
     }
+
+    s = av[1];
+    i = 0;
+
+    while (s[i])                 // sona git
+        i++;
+
+    i--;                         // son karakter
+
+    while (i >= 0 && is_space(s[i]))   // sondaki boşlukları at
+        i--;
+
+    if (i < 0)                   // tamamen boşluksa
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+
+    end = i;                     // son kelimenin sonu
+
+    while (i >= 0 && !is_space(s[i]))  // başını bul
+        i--;
+
+    start = i + 1;
+
+    while (start <= end)         // yazdır
+    {
+        write(1, &s[start], 1);
+        start++;
+    }
+
     write(1, "\n", 1);
     return (0);
 }
